edfd83492480c88d5dd9b8b8b32c08bda8f144c0
lixiang <lixiang2@nuctech.com>
2021-06-25 09:09:18 +0800

    finish_slam.sh: add mkdir

--- finish_slam_2d.sh
+++ finish_slam_2d.sh
@@ -5,6 +5,12 @@ source install_isolated/setup.bash
 map_dir="${HOME}/carto_ws/map"
 map_name="2d-1"
 
+# 检查文件夹是否存在, 如果不存在就创建文件夹
+if [ ! -d "$map_dir" ];then
+  echo "文件夹不存在, 正在创建文件夹"
+  mkdir -p $map_dir
+fi
+
 
 # finish slam
 rosservice call /finish_trajectory 0
